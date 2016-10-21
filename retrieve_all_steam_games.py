ca08e6932938409ba621a30870be2052ccdb620f
Irina Preda <irinapreda91@gmail.com>
2016-10-21 18:34:47 +0100

    adding script for list of all steam games

---
+++ retrieve_all_steam_games.py
@@ -0,0 +1,33 @@
+# get all steam games from steam api games list
+
+#from six.moves import urllib
+#import urllib2
+import json
+#page = urllib2.urlopen('http://api.steampowered.com/ISteamApps/GetAppList/v0001/')
+
+out = open('all_games.txt', 'w') # name id
+
+with open('GetAppList.json', 'r') as f:
+     js = json.load(f)
+
+no_of_oops = 0
+for i in js.keys():
+	for j in js[i]:
+		for x in js[i][j]:
+			for y in js[i][j][x]:
+				try:
+					out.write(str(y['name']))
+				except(UnicodeEncodeError):
+					for i in y['name']:
+						try:
+							out.write(i)
+						except(UnicodeEncodeError):
+							print "oops"
+							no_of_oops += 1
+				out.write(" ")
+				print y['appid']
+				out.write(str(y['appid']))
+				out.write('\n')
+
+out.close()
+print no_of_oops
